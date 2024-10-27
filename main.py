a302670801a14df60591289a6dbc2f0dfd00db52
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-10-26 23:26:18 -0300

    up

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permite todas as origens (modifique conforme necessário)
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc)
-    allow_headers=["*"],  # Permite todos os cabeçalhos
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get("/")
+# segunda consulta
+# SELECT * FROM tembo.tb_vendas
+
+# ------------------------------------------------------------------------------------------
+# TABELA PRODUTOS
+@app.get("/produtos")
 def load_database():
     host = 'gluttonously-bountiful-sloth.data-1.use1.tembo.io'
     database = 'postgres'
@@ -41,12 +46,39 @@ def load_database():
     return df.to_dict(orient="records")
 
 
+# ------------------------------------------------------------------------------------------
+# TABELA VENDAS
+
+@app.get("/vendas")
+def load_tbvendas():
+    host = 'gluttonously-bountiful-sloth.data-1.use1.tembo.io'
+    database = 'postgres'
+    user = 'postgres'
+    password = 'MeSaIkkB57YSOgLO'
+    port = '5432'
+
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            port=port
+        )        
+        query = "SELECT * FROM tembo.tb_vendas;"
+        df = pd.read_sql_query(query, conn)
+    except Exception as e:
+  
+    
+
+     if conn:
+        conn.close()
+    return df.to_dict(orient="records")
+
+# ------------------------------------------------------------------------------------------
+
 # uvicorn main:app --reload
 
 # uvicorn main:app --host 0.0.0.0 --port 10000
 
 # fastapi dev main.py rodar no terminal para ver local
-
-# ------------------------------------------------------------------------------------------
-
-
